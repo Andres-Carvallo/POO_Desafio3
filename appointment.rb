071237cf56b271f6847666bcf74279b821203d56
Andrés <andcarv89@gmail.com>
2020-08-10 20:19:36 -0400

    Fixing grammar erros

--- appointment.rb
+++ appointment.rb
@@ -1,8 +1,8 @@
 class Appointment
-    attr_accessor :location, :propuse, :hour, :min
-    def initialize (location, prupose, hour, min)
+    attr_accessor :location, :purpose, :hour, :min
+    def initialize (location, purpose, hour, min)
         @location = location
-        @prupose = propuse
+        @purpose = purpose
         @hour = hour
         @min = min
     end
@@ -10,11 +10,8 @@ end
 
 class MonthlyAppointment < Appointment
     attr_accessor :day
-    def initialize (location, prupose, hour, min, day)
-        @location = location
-        @prupose = propuse
-        @hour = hour
-        @min = min
+    def initialize (location, purpose, hour, min, day)
+        super(location, purpose, hour, min)
         @day = day
     end
     def occurs_on?(day)
@@ -22,7 +19,7 @@ class MonthlyAppointment < Appointment
     end
 
     def to_s
-        puts "Reunión mensual en #{@location} sobre #{@prupose} el día #{@day} a la(s) #{@hour}:#{@min}."
+        puts "Reunión mensual en #{@location} sobre #{@purpose} el día #{@day} a la(s) #{@hour}:#{@min}."
     end
 end
 
@@ -31,13 +28,14 @@ class DailyAppointment < Appointment
         puts @hour == hour && @min == min
     end
     def to_s
-        puts "Reunión diaria en #{@location} sobre #{@prupose} a la(s) #{@hour}:#{@min}."
+        puts "Reunión diaria en #{@location} sobre #{@purpose} a la(s) #{@hour}:#{@min}."
     end
 end
 
 class OneTimeAppointment < Appointment
     attr_accessor :day, :month, :year
-    def initialize (location, prupose, hour, min, day, month, year)
+    def initialize (location, purpose, hour, min, day, month, year)
+        super(location, purpose, hour, min)
         @day = day
         @month = month
         @year = year
@@ -46,27 +44,26 @@ class OneTimeAppointment < Appointment
         puts @day == day && @month == month && @year == year
     end
     def to_s
-        puts "Reunión única en #{@location} sobre #{@prupose} el #{@day}/#{@month}/#{@yeat} la(s) #{@hour}:#{@min}." 
+        puts "Reunión única en #{@location} sobre #{@purpose} el #{@day}/#{@month}/#{@year} la(s) #{@hour}:#{@min}." 
     end
 end
     
 
 
-puts OneTimeAppointment.new('Desafío Latam', ' Trabajo', 14,30,4,6,2019).to_s
-puts DailyAppointment.new('Desafío Latam', ' Educación', 8,15)
+puts OneTimeAppointment.new('Desafío Latam', 'Trabajo', 14,30,4,6,2019).to_s
+puts DailyAppointment.new('Desafío Latam', 'Educación', 8,15).to_s
 puts MonthlyAppointment.new('NASA', 'Aliens', 8, 15, 23).to_s
 
 
-ap3 = MonthlyAppointment.new('Desafío Latam', ' Trabajo', 14,30,6)
+ap3 = MonthlyAppointment.new('Desafío Latam', 'Trabajo', 14,30,6)
 ap3.occurs_on?(6)
 
-ap2 = DailyAppointment.new('Desafío Latam', ' Trabajo', 14,30)
+ap2 = DailyAppointment.new('Desafío Latam', 'Trabajo', 14,30)
 ap2.occurs_on?(14,30)
 
 ap1 = OneTimeAppointment.new('Desafío Latam', ' Trabajo', 14,30,4,6,2019)
 ap1.occurs_on?(4,6,2018)
 
-ap2.to_s
 
 
 
